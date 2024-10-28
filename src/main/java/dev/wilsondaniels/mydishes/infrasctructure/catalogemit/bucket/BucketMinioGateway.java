5edcc2e981d553dafbbfc07b0886fd7f9e9719a4
Wilson Daniel da Silva <wilsondaniels@gmail.com>
2024-10-28 12:33:02 -0300

    feat: add first version for upload files by minio

--- src/main/java/dev/wilsondaniels/mydishes/infrasctructure/catalogemit/bucket/BucketMinioGateway.java
+++ src/main/java/dev/wilsondaniels/mydishes/infrasctructure/catalogemit/bucket/BucketMinioGateway.java
@@ -1,12 +1,49 @@
 package dev.wilsondaniels.mydishes.infrasctructure.catalogemit.bucket;
 
 import dev.wilsondaniels.mydishes.domain.catalogemit.BucketGateway;
+import dev.wilsondaniels.mydishes.domain.exception.DomainException;
+import dev.wilsondaniels.mydishes.domain.validation.Error;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
 
 @Service
 public class BucketMinioGateway implements BucketGateway {
+
+    private final MinioClient minioClient;
+
+    @Value("${minio.bucketName}")
+    private String bucketName;
+
+    public BucketMinioGateway(MinioClient minioClient) {
+        this.minioClient = minioClient;
+    }
+
     @Override
     public void upload(String anOwnerId, String catalog) {
-        System.err.println("uploaded....\nowner: " + anOwnerId + "\n" + catalog);
+        try {
+            uploadFileFromString(catalog, anOwnerId + "-catalog.json");
+        } catch (Exception e) {
+            throw DomainException.with(new Error("Error sending file to server."));
+        }
+    }
+
+
+    private void uploadFileFromString(String content, String fileName) throws Exception {
+        final ByteArrayInputStream inputStream = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
+
+        minioClient.putObject(
+                PutObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(fileName)
+                        .stream(inputStream, inputStream.available(), -1)
+                        .contentType("application/json")
+                        .build()
+        );
     }
 }
---
+++ src/main/java/dev/wilsondaniels/mydishes/infrasctructure/configuration/MinioConfig.java
@@ -0,0 +1,27 @@
+package dev.wilsondaniels.mydishes.infrasctructure.configuration;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.url}")
+    private String minioUrl;
+
+    @Value("${minio.accessKey}")
+    private String accessKey;
+
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioUrl)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
